d05735fb9b6cd1b290bde75f07a8de7591416dc8
Kalyan Cheerla <32354220+kalyancheerla@users.noreply.github.com>
2025-07-09 14:25:33 -0500

    added redirects for certain pages

---
+++ src/data/redirects.ts
@@ -0,0 +1,8 @@
+export const redirects: Record<string, string> = {
+  resume: "https://drive.google.com/file/d/1_v3nzW9A_jMF75MU81UUqdNfKGiaWJxF/view?usp=sharing",
+  github: "https://github.com/kalyancheerla",
+  linkedin: "https://www.linkedin.com/in/kalyancheerla",
+  microsvcsthreatsmapper: "https://github.com/kalyancheerla/threatsmanager/tree/MicroSvcsThreatsMapper_Tool/Tools/MicroSvcsThreatsMapper#video",
+  MicroSvcsThreatsMapper: "https://github.com/kalyancheerla/threatsmanager/tree/MicroSvcsThreatsMapper_Tool/Tools/MicroSvcsThreatsMapper#video",
+  HotelCancellationsML: "https://github.com/kalyancheerla/Hotel-Reservation-Cancellation-Prediction",
+};
