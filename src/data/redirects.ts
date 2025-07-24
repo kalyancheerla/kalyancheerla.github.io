1d1c5559fccab4ca3d7419518a103b7712daee89
Kalyan Cheerla <32354220+kalyancheerla@users.noreply.github.com>
2025-07-24 02:02:35 -0500

    added redirect for ppt

--- src/data/redirects.ts
+++ src/data/redirects.ts
@@ -5,4 +5,5 @@ export const redirects: Record<string, string> = {
   microsvcsthreatsmapper: "https://github.com/kalyancheerla/threatsmanager/tree/MicroSvcsThreatsMapper_Tool/Tools/MicroSvcsThreatsMapper#video",
   MicroSvcsThreatsMapper: "https://github.com/kalyancheerla/threatsmanager/tree/MicroSvcsThreatsMapper_Tool/Tools/MicroSvcsThreatsMapper#video",
   HotelCancellationsML: "https://github.com/kalyancheerla/Hotel-Reservation-Cancellation-Prediction",
+  ppt: "https://docs.google.com/presentation/d/1bA3Pf4JRuQ1kfrIacaB7FasDd3krhH66/edit?usp=sharing&ouid=103134828204008657489&rtpof=true&sd=true",
 };
