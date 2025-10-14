373288095a7963dce7e9ee20abb0d35e80fc45ee
Kalyan Cheerla <32354220+kalyancheerla@users.noreply.github.com>
2025-10-13 22:04:57 -0500

    added ppt redirect for secdev ppt

--- src/data/redirects.ts
+++ src/data/redirects.ts
@@ -5,7 +5,7 @@ export const redirects: Record<string, string> = {
   microsvcsthreatsmapper: "https://github.com/kalyancheerla/threatsmanager/tree/MicroSvcsThreatsMapper_Tool/Tools/MicroSvcsThreatsMapper#video",
   MicroSvcsThreatsMapper: "https://github.com/kalyancheerla/threatsmanager/tree/MicroSvcsThreatsMapper_Tool/Tools/MicroSvcsThreatsMapper#video",
   HotelCancellationsML: "https://github.com/kalyancheerla/Hotel-Reservation-Cancellation-Prediction",
-  ppt: "https://1drv.ms/p/c/77f7f1145d1e1253/EcOowvxNJSpGhWu3lCTwPl0B56u9hRMShHZKMOzPfZKJKg",
+  ppt: "https://1drv.ms/p/c/77f7f1145d1e1253/ESfoT9MZrZpAl4CHjDMfG8gBMswOSMypS0NINUNeKycvEA",
   voxpilot: "https://kalyanch.com/posts/voxpilot-client-side-genai/",
   cv: "https://drive.google.com/file/d/1h6IM3oG1nUVdntx6UBod15b28DyVmW2K/view?usp=sharing",
 };
